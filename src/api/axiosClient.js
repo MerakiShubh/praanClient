a4579a0492d2b4a11da5009ce8dff79163d40e79
MerakiShubh <shubhamt.champ@gmail.com>
2024-10-30 01:23:14 +0530

    save the changes

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { store } from "../app/store";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:5000/api/v1",
+  baseURL: "https://praanserver.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
